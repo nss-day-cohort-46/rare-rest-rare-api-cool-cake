593bf4b322b28a45431e7bd283c52f3d3021f9aa
daprieu <daprieu@gmail.com>
2021-05-17 13:39:52 -0500

    hot fix to models for migrations to work

--- rareapi/models/demotionqueue.py
+++ rareapi/models/demotionqueue.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class DemotionQueue(models.Model):
-    action = models.CharField()
-    admin = models.ForeignKey("RareUser", on_delete=models.CASCADE)
-    approver_one = models.ForeignKey("RareUser", on_delete=models.CASCADE)  
+    action = models.CharField(max_length=50)
+    admin = models.ForeignKey("RareUser", on_delete=models.CASCADE, related_name="admin")
+    approver_one = models.ForeignKey("RareUser", on_delete=models.CASCADE, related_name="approver_one")  
