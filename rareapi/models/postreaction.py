e6a3f526613fddb8879ee189787dc0eb45830e64
Dan Ross <daniellross@gmail.com>
2021-05-17 14:23:34 -0500

    loaded fixtures, updated fixtures

--- rareapi/models/postreaction.py
+++ rareapi/models/postreaction.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class PostReaction(models.Model):
-    user = models.OneToOneField("RareUser", on_delete=models.CASCADE)
-    post = models.OneToOneField("Post", on_delete=models.CASCADE)
-    reaction = models.OneToOneField("Reaction", on_delete=models.CASCADE)
+    user = models.ForeignKey("RareUser", on_delete=models.CASCADE)
+    post = models.ForeignKey("Post", on_delete=models.CASCADE)
+    reaction = models.ForeignKey("Reaction", on_delete=models.CASCADE)
