e94ebf6680cd24cc1677b44ce3084f464f155a71
CheoR <cheo.roman+github@gmail.com>
2021-05-17 14:49:39 -0500

    fix: update migrations to reflect subscriptions model update.

--- rareapi/models/subscription.py
+++ rareapi/models/subscription.py
@@ -1,7 +1,10 @@
 from django.db import models
 
+
 class Subscription(models.Model):
-    follower = models.ForeignKey("RareUser", on_delete=models.CASCADE, related_name="follower")
-    author = models.ForeignKey("RareUser", on_delete=models.CASCADE, related_name="author")
+    follower = models.ForeignKey(
+        "RareUser", on_delete=models.CASCADE, related_name="follower")
+    author = models.ForeignKey(
+        "RareUser", on_delete=models.CASCADE, related_name="author")
     created_on = models.DateTimeField()
-    ended_on = models.DateTimeField()
+    ended_on = models.DateTimeField(blank=True, null=True)
