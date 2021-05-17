21c8c2b3838578f90f9f73522ebcf89d5309f518
phillipphan <itsphillipphan@gmail.com>
2021-05-17 13:40:30 -0500

    merging main

--- rareapi/views/comment.py
+++ rareapi/views/comment.py
@@ -1,12 +1,30 @@
 from rest_framework.viewsets import ViewSet
-from rareapi.models import RareUser, Comment
+from rest_framework.response import Response
+from rest_framework import serializers
+from rest_framework import status
+from django.core.exceptions import ValidationError
+from rareapi.models import RareUser, Comment, Post
 
 class Comment(ViewSet):
     def create(self, request):
         author = RareUser.objects.get(user = request.auth.user)
+        post = Post.objects.get(pk = request.data["postId"])
 
         comment = Comment()
         comment.content = request.data["content"]
         comment.created_on = request.data["createdOn"]
         comment.author = author
-        
+        comment.post = post
+
+        try:
+            comment.save()
+            serializer = CommentSerializer(comment, context={'request': request})
+            return Response(serializer.data)
+
+        except ValidationError as ex:
+            return Response({"reason": ex.message}, status=status.HTTP_404_BAD_REQUEST)
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ('id', 'post', 'author', 'content', 'create_on')
