b176b733a2acd729203ad8d1b6807f95560ef1f5
phillipphan <itsphillipphan@gmail.com>
2021-05-17 15:07:47 -0500

    [feat]: backend for creating comments is complete.

--- rareapi/models/subscription.py
+++ rareapi/models/subscription.py
@@ -4,4 +4,4 @@ class Subscription(models.Model):
     follower = models.ForeignKey("RareUser", on_delete=models.CASCADE, related_name="follower")
     author = models.ForeignKey("RareUser", on_delete=models.CASCADE, related_name="author")
     created_on = models.DateTimeField()
-    ended_on = models.DateTimeField()
+    ended_on = models.DateTimeField(blank=True, null=True)
--- rareapi/views/comment.py
+++ rareapi/views/comment.py
@@ -5,7 +5,7 @@ from rest_framework import status
 from django.core.exceptions import ValidationError
 from rareapi.models import RareUser, Comment, Post
 
-class Comment(ViewSet):
+class Comments(ViewSet):
     def create(self, request):
         author = RareUser.objects.get(user = request.auth.user)
         post = Post.objects.get(pk = request.data["postId"])
@@ -20,11 +20,10 @@ class Comment(ViewSet):
             comment.save()
             serializer = CommentSerializer(comment, context={'request': request})
             return Response(serializer.data)
-
         except ValidationError as ex:
             return Response({"reason": ex.message}, status=status.HTTP_404_BAD_REQUEST)
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = ('id', 'post', 'author', 'content', 'create_on')
+        fields = ('id', 'post', 'author', 'content', 'created_on')
