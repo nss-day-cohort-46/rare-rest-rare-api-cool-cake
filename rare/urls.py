9de3fdf064ece5c4d78974af1fb4d1960e66b224
phillipphan <itsphillipphan@gmail.com>
2021-05-17 15:40:00 -0500

    [feat]: backend for creating new reactions is completed

--- rare/urls.py
+++ rare/urls.py
@@ -18,10 +18,11 @@ from django.conf.urls import include
 from django.urls import path
 from rareapi.views import register_user, login_user
 from rest_framework import routers
-from rareapi.views import Comments
+from rareapi.views import Comments, Reactions
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'comments', Comments, 'comment')
+router.register(r'reactions', Reactions, 'reaction')
 
 urlpatterns = [
     path('', include(router.urls)),
--- rareapi/views/__init__.py
+++ rareapi/views/__init__.py
@@ -1,3 +1,4 @@
 from .auth import login_user
 from .auth import register_user
 from .comment import Comments
+from .reaction import Reactions
