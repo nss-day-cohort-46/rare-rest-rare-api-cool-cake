6e9404cb905f7f448244e22403351a45b508ce78
CheoR <cheo.roman+github@gmail.com>
2021-05-17 14:21:11 -0500

    fix: from phil update via slack.

--- rareapi/models/postreaction.py
+++ rareapi/models/postreaction.py
@@ -1,6 +1,7 @@
 from django.db import models
 
+
 class PostReaction(models.Model):
     user = models.OneToOneField("RareUser", on_delete=models.CASCADE)
     post = models.OneToOneField("Post", on_delete=models.CASCADE)
-    reaction = models.OneToOneField("Reaction", on_delete=models.CASCADE)
+    reaction = models.ForeignKey("Reaction", on_delete=models.CASCADE)
