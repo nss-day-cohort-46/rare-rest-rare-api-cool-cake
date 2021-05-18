b844e56f6b8bac635100037f5c6a66496da5776e
phillipphan <itsphillipphan@gmail.com>
2021-05-18 10:36:43 -0500

    [feat]: backend for adding reactions to posts is complete

--- rareapi/views/post.py
+++ rareapi/views/post.py
@@ -1,11 +1,12 @@
 """View module for handling requests about games"""
 from django.core.exceptions import ValidationError
+from rest_framework.decorators import action
 from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
 from rest_framework import status
-from rareapi.models import Post
+from rareapi.models import Post, RareUser, PostReaction, Reaction
 
 
 
@@ -39,6 +40,57 @@ class PostView(ViewSet):
         serializer = PostSerializer(
             post, many=True, context={'request': request})
         return Response(serializer.data)
+    
+    @action(methods=['post', 'delete'], detail=True)
+    def react(self, request, pk=None):
+        if request.method == "POST":
+            post = Post.objects.get(pk=pk)
+            user = RareUser.objects.get(user=request.auth.user)
+            reaction = Reaction.objects.get(pk = request.data['reactionId']) 
+
+            try:
+                reacting = PostReaction.objects.get(
+                    user=user, post=post, reaction=reaction
+                )
+                return Response(
+                    {'message': 'User already used this reaction.'},
+                    status=status.HTTP_422_UNPROCESSABLE_ENTITY
+                )
+            except PostReaction.DoesNotExist:
+                reacting = PostReaction()
+                reacting.post = post
+                reacting.user = user
+                reacting.reaction = reaction
+                reacting.save()
+
+                return Response({}, status=status.HTTP_201_CREATED)
+        
+        elif request.method == "DELETE":
+            try:
+                post = Post.objects.get(pk=pk)
+            except Post.DoesNotExist:
+                return Response(
+                    {'message': 'Post does not exist.'},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+
+            user = RareUser.objects.get(user=request.auth.user)
+            reaction = Reaction.objects.get(pk = request.data['reactionId'])
+
+            try:
+                reacting = PostReaction.objects.get(
+                    user=user, post=post, reaction=reaction
+                )
+                reacting.delete()
+                return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+            except PostReaction.DoesNotExist:
+                return Response(
+                    {'message': 'User has not used this reaction.'},
+                    status=status.HTTP_404_NOT_FOUND
+                )
+
+        return Response({}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 class PostSerializer(serializers.ModelSerializer):
     """JSON serializer for posts
