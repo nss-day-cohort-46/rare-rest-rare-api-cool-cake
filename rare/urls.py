6ac52864622178ae0a06d9dd230b05254ddaf3dd
Dan Ross <daniellross@gmail.com>
2021-05-18 16:48:14 -0500

    added path for profiles. only admin users can make get request for profiles

--- rare/urls.py
+++ rare/urls.py
@@ -1,18 +1,15 @@
 from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
-
 from rest_framework import routers
-
 from rareapi.views import register_user, login_user
-from rareapi.views.tagViewSet import TagViewSet
-
-from rareapi.views import Comments, Reactions
+from rareapi.views import Comments, Reactions, TagViewSet, ProfileView
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'comments', Comments, 'comment')
 router.register(r'reactions', Reactions, 'reaction')
 router.register(r'tags', TagViewSet, 'tag')
+router.register(r'profiles', ProfileView, 'profile')
 
 urlpatterns = [
     path('', include(router.urls)),
