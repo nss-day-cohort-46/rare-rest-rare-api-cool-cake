ea9924172c52ad8b2cc7f064436179f164a794f9
daprieu <daprieu@gmail.com>
2021-05-18 14:25:20 -0500

    users can not edit or delete other posts

--- rareapi/views/post.py
+++ rareapi/views/post.py
@@ -1,12 +1,16 @@
 """View module for handling requests about games"""
+from rareapi.models.postreaction import PostReaction
+from rareapi.models.posttag import PostTag
+from rareapi.models.reaction import Reaction
+from rareapi.models.tag import Tag
+from rareapi.models.category import Category
 from django.core.exceptions import ValidationError
 from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
 from rest_framework import status
-from rareapi.models import Post
-from django.contrib.auth.models import User
+from rareapi.models import Post, RareUser
 
 
 
@@ -40,6 +44,78 @@ class PostView(ViewSet):
         serializer = PostSerializer(
             post, many=True, context={'request': request})
         return Response(serializer.data)
+
+    def create(self, request):
+        """Handle POST operations
+        Returns:
+            Response -- JSON serialized game instance
+        """
+        # Uses the token passed in the `Authorization` header
+        user = RareUser.objects.get(user=request.auth.user)
+        category = Category.objects.get(pk = request.data["categoryId"])
+        # tags = PostTag.objects.get(pk=request.data["tagId"])
+        # reactions = PostReaction.objects.get(pk=request.data["reactionId"])
+
+        post = Post()
+        post.user = user
+        post.category = category
+        post.title = request.data["title"]
+        post.publication_date = request.data["publishedOn"]
+        post.image_url = request.data["imageUrl"]
+        post.content = request.data["content"]
+        post.approved = request.data["approved"]
+        # post.tags = tags
+        # post.reactions = reactions
+
+        try:
+            post.save()
+            serializer = PostSerializer(post, context={'request': request})
+            return Response(serializer.data)
+        except ValidationError as ex:
+            return Response({"reason": ex.message}, status=status.HTTP_404_BAD_REQUEST)
+
+    def update(self, request, pk=None):
+        """Handle PUT requests for a game
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+        user = RareUser.objects.get(user=request.auth.user)
+        category = Category.objects.get(pk = request.data["categoryId"])
+        post = Post.objects.get(pk=pk)
+
+
+        if user is not post.user:
+            return Response({}, status=status.HTTP_403_FORBIDDEN)
+
+        post.user = user
+        post.category = category
+        post.title = request.data["title"]
+        post.publication_date = request.data["publishedOn"]
+        post.image_url = request.data["imageUrl"]
+        post.content = request.data["content"]
+        post.approved = request.data["approved"]
+
+        post.save()
+
+        # 204 status code means everything worked but the
+        # server is not sending back any data in the response
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+    def destroy(self, request, pk=None):
+
+        try:
+            post = Post.objects.get(pk=pk)
+            user = RareUser.objects.get(user=request.auth.user)
+            if user is not post.user:
+                return Response({}, status=status.HTTP_403_FORBIDDEN)
+            post.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+        except Post.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 # class UserSerializer(serializers.ModelSerializer):
 #     """JSON serializer for gamer's related Django user"""
