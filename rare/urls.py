4a411a803ac2286c75c99d50d67cb80461546451
Dan Ross <daniellross@gmail.com>
2021-05-18 15:08:23 -0500

    new branch. fixed weird merge conflict and settings.json

--- rare/urls.py
+++ rare/urls.py
@@ -1,40 +1,15 @@
-"""rare URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
 from rest_framework import routers
-<<<<<<< HEAD
-from rareapi.views import Comments, Reactions
+from rareapi.views import Comments, Reactions, TagViewSet
+from rareapi.views import register_user, login_user
+
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'comments', Comments, 'comment')
 router.register(r'reactions', Reactions, 'reaction')
-=======
-
-from rareapi.views import register_user, login_user
-from rareapi.views import Comments
-from rareapi.views.tagViewSet import TagViewSet
-
-router = routers.DefaultRouter(trailing_slash=False)
-router.register(r'comments', Comments, 'comment')
 router.register(r'tags', TagViewSet, 'tag')
-
->>>>>>> main
 
 urlpatterns = [
     path('', include(router.urls)),
